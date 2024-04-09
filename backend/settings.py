61fdfa159f31a974b584d1e893f714feb2bbba31
Pranav <pranavg456@gmail.com>
2024-04-09 20:43:04 +0530

    update test_project workflow

--- backend/settings.py
+++ backend/settings.py
@@ -16,7 +16,7 @@ from pathlib import Path
 from dotenv import load_dotenv
 
 
-load_dotenv(".env-local")
+load_dotenv(".env.prod")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
