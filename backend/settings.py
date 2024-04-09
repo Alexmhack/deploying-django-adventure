e6f264c1f7fcc60c03cef2562747cd3c8a4e2b96
Pranav <pranavg456@gmail.com>
2024-04-09 19:13:04 +0530

    setup postgres on docker and azure with init.sql

--- backend/settings.py
+++ backend/settings.py
@@ -13,6 +13,10 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 import os
 
 from pathlib import Path
+from dotenv import load_dotenv
+
+
+load_dotenv(".env-local")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -92,7 +96,7 @@ if os.getenv("DATABASE_BACKEND") == "postgres":
     DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.postgresql",
-            "NAME": os.getenv("POSTGRES_DB_NAME"),
+            "NAME": os.getenv("POSTGRES_DB"),
             "USER": os.getenv("POSTGRES_USER"),
             "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
             "HOST": os.getenv("POSTGRES_HOST"),
